84d3db80a826e5322635a1aedcefc03e73b4ea1e
mhrusz <mhrusz+github@gmail.com>
2016-07-18 14:49:04 +0200

    Some more complicated examples added.

--- fortran_c_calls/demo.f90
+++ fortran_c_calls/demo.f90
@@ -8,9 +8,20 @@ PROGRAM demo
       ! https://www.pgroup.com/support/interlan.htm 
       ! + intel
       ! https://software.intel.com/en-us/node/525330 
+      ! 
+      ! TYPES:
+      ! https://gcc.gnu.org/onlinedocs/gfortran/ISO_005fC_005fBINDING.html#ISO_005fC_005fBINDING 
 
       USE iso_c_binding
       IMPLICIT NONE
+
+      INTEGER :: summ,i,j
+      INTEGER, PARAMETER :: N = 10
+      INTEGER,DIMENSION(N) :: A = (/1,2,3,4,5,6,7,8,9,0/)
+      REAL,DIMENSION(N,N) :: B = RESHAPE((/(i*0.5, i=1,100)/), (/N, N/))
+      REAL,DIMENSION(N,N) :: C = RESHAPE((/(i*0.5, i=1,100)/), (/N, N/))
+      TYPE(C_PTR) :: D
+      REAL, POINTER :: DE(N,N)
       
       ! C funcion names are case sensitive!
       ! interface for C function must be provided
@@ -23,6 +34,56 @@ PROGRAM demo
           END SUBROUTINE sampprint
       END INTERFACE
 
+      INTERFACE
+          INTEGER(kind=C_INT) FUNCTION sampadd(a, b) BIND(C, name="samp_add")
+              USE iso_c_binding, ONLY: C_INT
+              INTEGER(kind=C_INT),VALUE :: a
+              INTEGER(kind=C_INT),VALUE :: b
+          END FUNCTION sampadd
+      END INTERFACE
+
+      INTERFACE
+          INTEGER(kind=C_INT) FUNCTION sampaddp(a, b) BIND(C, name="samp_addp")
+              USE iso_c_binding, ONLY: C_INT
+              INTEGER(kind=C_INT) :: a
+              INTEGER(kind=C_INT) :: b
+          END FUNCTION sampaddp
+      END INTERFACE
+
+      INTERFACE
+          SUBROUTINE sampprintarr(a, n) BIND(C, name="samp_print_arr")
+              USE iso_c_binding, ONLY: C_INT
+              INTEGER(kind=C_INT), DIMENSION(n) :: a
+              INTEGER(kind=C_INT),VALUE :: n
+          END SUBROUTINE sampprintarr
+      END INTERFACE
+
+      INTERFACE
+          SUBROUTINE sampmult(a, b, c, n) BIND(C, name="samp_mult")
+              USE iso_c_binding, ONLY: C_INT, C_FLOAT, C_PTR
+              REAL(kind=C_FLOAT), DIMENSION(n,n) :: a
+              REAL(kind=C_FLOAT), DIMENSION(n,n) :: b
+              TYPE(C_PTR), INTENT(OUT) :: c
+              INTEGER(kind=C_INT),VALUE :: n
+          END SUBROUTINE sampmult
+      END INTERFACE
+
       CALL sampprint(10)
+      summ = sampadd(1, 2)
+      WRITE(*,*) 'Sum by sampadd is',summ
+      summ = sampaddp(1, 2)
+      WRITE(*,*) 'Sum by sampaddp (with pointers) is',summ
+      CALL sampprintarr(A, N)
+
+      WRITE(*,*) 'ee'
+      CALL sampmult(B, C, D, N)
+
+      WRITE(*,*) 'eeeeeee'
+      CALL C_F_POINTER(D, DE, (/N,N/))
+
+      !DO, i=1,N
+      !    WRITE(*,*) ( C(i,j), j=1,N )
+      !ENDDO
+
 
 END PROGRAM demo
--- fortran_c_calls/lib_c.c
+++ fortran_c_calls/lib_c.c
@@ -1,14 +1,64 @@
 /*******************************************************************************
 *  File Name           :     lib_c.c
-*  Created By          :     Mariusz Hruszowiec <mariusz.hruszowiec@pwr.edu.pl>
+*  Created By          :     mhrusz <mhrusz+github@gmail.com>
 *  Creation Date       :     [2016-07-18 12:59]
-*  Last Modified       :     [2016-07-18 13:33]
+*  Last Modified       :     [2016-07-18 14:49]
 *  Description         :     Sample library 
 ********************************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+ * Sample print function.
+ */
 void sampPrint(int a)
 {
     printf("Hello world %d!!!\n", a);
 };
+
+/*
+ * Adding two values by value and return results
+ */
+int samp_add(int a, int b)
+{
+    return a + b;
+}
+
+/*
+ * Adding two values by pointer
+ */
+int samp_addp(int *a, int *b)
+{
+    return *a + *b;
+}
+
+/* 
+ * Passing whole array to print
+ */
+void samp_print_arr(int *a, int size)
+{
+    for(unsigned i=0; i<size; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+}
+
+/*
+ * Array multiplication,
+ * assuming all dimensions of three matrices are the same
+ */
+void samp_mult(float **a, float **b, float **c, int n)
+{
+    float sum=0.0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            for(int k=0; k< n; k++)
+                sum = sum + a[i][k] * b[k][j];
+
+            c[i][j] = sum;
+            sum = 0.0;
+        }
+    }
+}
