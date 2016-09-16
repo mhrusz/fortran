dd48c3eb7fff98d045affbe23fd0a0407f7590b0
mhrusz <mhrusz+github@gmail.com>
2016-09-16 10:12:54 +0200

    Printing complex array.

--- fortran_complex_c_operations/lib_c.c
+++ fortran_complex_c_operations/lib_c.c
@@ -2,7 +2,7 @@
 *  File Name           :     lib_c.c
 *  Created By          :     mhrusz <mhrusz+github@gmail.com>
 *  Creation Date       :     [2016-07-18 12:59]
-*  Last Modified       :     [2016-09-16 09:36]
+*  Last Modified       :     [2016-09-16 10:12]
 *  Description         :     Operations on complex values
 ********************************************************************************/
 #include <stdlib.h>
@@ -20,5 +20,18 @@ typedef struct
 void cmplx_print_(complex *N)
 {
   printf("complex: (%f, %f)\n", N->real, N->imag);
+}
+
+/*
+ * Displaying complex array.
+ */
+void cmplx_print_arr_(const complex *A, const int *N)
+{
+  for(int i=0; i< *N; ++i)
+  {
+    for(int j=0; j< *N; ++j)
+      printf("(%f, %f)\n", A[j+i* *N].real, A[j+i* *N].imag);
+    printf("\n");
+  }
 }
 
